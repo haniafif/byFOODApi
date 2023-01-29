85897adcc084da33c84ef7458b9fea0cc19b33fd
Hani <haniaboultaif@gmail.com>
2023-01-29 12:32:29 -0300

    First commit, create Java + SpringBoot + Maven

---
+++ src/main/java/com/example/byFOOD/byFOODapp.java
@@ -0,0 +1,13 @@
+package com.example.byFOOD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class byFOODapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(byFOODapp.class, args);
+		System.out.println("Hello world");
+	}
+}
